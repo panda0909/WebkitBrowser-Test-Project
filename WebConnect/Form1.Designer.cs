d03d54cd8e9f4f402881f16f17bcfc2e5f0997f5
PanHongMing <40025146@gm.nfu.edu.tw>
2018-04-17 12:11:01 +0800

    測試Webkit browser控制

--- WebConnect/Form1.Designer.cs
+++ WebConnect/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// 設計工具所需的變數。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清除任何使用中的資源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,21 +20,67 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 設計工具產生的程式碼
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
-        /// 這個方法的內容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webKitBrowser1 = new WebKit.WebKitBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webKitBrowser1
+            // 
+            this.webKitBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webKitBrowser1.BackColor = System.Drawing.Color.White;
+            this.webKitBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webKitBrowser1.Name = "webKitBrowser1";
+            this.webKitBrowser1.Size = new System.Drawing.Size(1029, 478);
+            this.webKitBrowser1.TabIndex = 0;
+            this.webKitBrowser1.Url = null;
+            this.webKitBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webKitBrowser1_DocumentCompleted);
+            this.webKitBrowser1.Load += new System.EventHandler(this.webKitBrowser1_Load);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(913, 484);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1029, 529);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webKitBrowser1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "手動領號";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private WebKit.WebKitBrowser webKitBrowser1;
+        private System.Windows.Forms.Button button1;
     }
 }
-
--- WebConnect/Form1.cs
+++ WebConnect/Form1.cs
@@ -16,5 +16,63 @@ namespace WebConnect
         {
             InitializeComponent();
         }
+        public string LastUrl { set; get; }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webKitBrowser1.Navigate("http://192.168.149.1/ArasMVC01/Login");
+            webKitBrowser1.DocumentCompleted += SetArasLogin;
+        }
+
+        private void webKitBrowser1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webKitBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+        private void SetArasLogin(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webKitBrowser1.Url.ToString() == LastUrl) return;
+            if (e.Url.ToString() != DocCompleteUrl.Login) return;
+            LastUrl = webKitBrowser1.Url.ToString();
+
+            List<string> loginTag = new List<string>() {"ip","db","admin","pwd" };
+            List<string> loginInfo = new List<string>() { "http://192.168.149.1/plm", "InnovatorSolutions", "admin", "innovator" };
+            for(int i =0; i < loginInfo.Count(); i++)
+            {
+                WebKit.DOM.Element element = webKitBrowser1.Document.GetElementById(loginTag[i]);
+                element.SetAttribute("value", loginInfo[i]);
+            }
+           
+            webKitBrowser1.StringByEvaluatingJavaScriptFromString("document.getElementById('loginSubmit').click();");
+            webKitBrowser1.DocumentCompleted += RunDocumentCompleted;
+            //webKitBrowser1.Navigate(DocCompleteUrl.AdminSearch);
+        }
+        private void RunDocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.ToString() != DocCompleteUrl.AdminSearch)
+            {
+                webKitBrowser1.Navigate(DocCompleteUrl.AdminSearch);
+                webKitBrowser1.DocumentCompleted += AdminSearchCompleted;
+            }
+        }
+        private void AdminSearchCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webKitBrowser1.Url.ToString() == LastUrl) return;
+            if (e.Url.ToString() != DocCompleteUrl.AdminSearch) return;
+            webKitBrowser1.Document.GetElementById("btnGetNumber").SetAttribute("type", "hidden");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(webKitBrowser1.Document.GetElementById("seq").TextContent);
+        }
+    }
+    public class DocCompleteUrl 
+    {
+        public static string Login { set; get; } = "http://192.168.149.1/ArasMVC01/Login";
+        public static string AdminSearch { set; get; } = "http://192.168.149.1/ArasMVC01/AdminSearch";
     }
 }
